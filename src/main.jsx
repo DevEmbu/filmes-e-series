a7e85d4cea4b88648234ad3ef27a593245b06f1d
DevEmbu <jideoliveira72@gmail.com>
2023-09-21 13:54:33 -0300

    nadanada

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import GlobalStyle  from './styles/globalStyles'
 
 import Rotas from './rootes/rootes.jsx'
 
@@ -10,5 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
      <BrowserRouter>
        <Rotas />
      </BrowserRouter>
+     <GlobalStyle />
   </React.StrictMode>,
 )
