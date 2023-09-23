f38e916bdca64e8e5ad279577c27bc0d095d0843
DevEmbu <jideoliveira72@gmail.com>
2023-09-22 22:19:06 -0300

    alteracao

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,14 @@
 import axios from 'axios'
 
-// https://api.themoviedb.org/3/movie/popular?{movie_id} /images
+// https://api.themoviedb.org/3/movie/popular?language=en-US&page=1' \
 
 const api = axios.create ({
     baseURL:'https://api.themoviedb.org/3',
     params:{ 
           api_key:'199c90afabe374492787e7801dc85d9e',
-    language:'pt-BR',
-    pages: 1
+          language:'pt-BR',
+          page: 1
     }
 })
+
+export default api
